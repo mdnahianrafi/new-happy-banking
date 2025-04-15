099b3a82a8e485d91d9b97f3c7ec0c7d639b9af8
mdnahainrafi <nahian2555@gmail.com>
2025-04-15 08:01:15 +0600

    overflow y

--- src/components/MobileContainer.jsx
+++ src/components/MobileContainer.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const MobileContainer = ({children}) => {
   return (
-<div className="scrollbar-w w-full md:w-[375px] md:h-[665px] md:border-8 rounded-2xl relative overflow-y-scroll  bg-[#F4F8FB]">
+<div className="scrollbar-w w-full md:w-[375px] md:h-[665px] md:border-8 rounded-2xl relative overflow-y-auto  bg-[#F4F8FB]">
   {children}
 </div>
 
